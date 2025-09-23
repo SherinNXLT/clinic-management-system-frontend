46eab0c4f1394907807a6a3f2e2c119ec273592b
Durgaprasad0012 <durgaprasad0012@gmail.com>
2025-09-23 16:57:10 +0530

    To add sign in page static design

--- src/components/ui/landing_page/Login.tsx
+++ src/components/ui/landing_page/Login.tsx
@@ -1,9 +1,19 @@
 
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    // handleSignUp
+    const handleSignUp = (): void => {
+        alert()
+    }
+    return (
+        <div className="w-full h-screen flex justify-center items-center p-5 md:p-10">
+            <div className="w-full h-1/2 md:w-1/2 p-5 rounded-lg bg-black/5 text-black/20 grid place-items-center ">
+
+                <p className="font-bold color  text-2xl">Sign In</p>
+                <p className="text-sm">Are you in new <span className="text-blue-500 cursor-pointer" onClick={handleSignUp}>Sign Up</span></p>
+            </div>
+        </div>
+    )
 }
 
 export default Login
