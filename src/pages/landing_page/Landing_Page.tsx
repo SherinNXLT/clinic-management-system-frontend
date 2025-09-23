0552ffa2ef0fc914a7b155544cd2fabcfb43aa0b
Durgaprasad0012 <durgaprasad0012@gmail.com>
2025-09-23 15:58:55 +0530

    To add Login  page on landing page

--- src/pages/landing_page/Landing_Page.tsx
+++ src/pages/landing_page/Landing_Page.tsx
@@ -1,9 +1,12 @@
 import LandingLayout from "../../components/layout/LandingLayout"
+import Login from "../../components/ui/landing_page/Login"
 
 
 const Landing_Page = () => {
     return (
-        <LandingLayout>Landing_Page</LandingLayout>
+        <LandingLayout>
+            <Login />
+        </LandingLayout>
     )
 }
 
