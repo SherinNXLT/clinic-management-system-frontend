b308d7e6198cc570dd0a9f52a3ffec3a77a4e4b6
Durgaprasad0012 <durgaprasad0012@gmail.com>
2025-09-24 16:32:04 +0530

    To add the new design and basic implimentation of login

--- src/components/ui/landing_page/Login.tsx
+++ src/components/ui/landing_page/Login.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import CommonInput from "../../common/CommonInput";
-import { FiLock, FiMail } from "react-icons/fi";
 import { validateSignIn } from "../../../utils/Validation";
 import { useNavigate } from "react-router-dom";
 import { BiSolidBank } from "react-icons/bi";
+import { FaLock, FaUser } from "react-icons/fa";
 type CredentialInfo = {
     email: string;
     password: string;
+    isCheck: boolean;
 };
 
 const Login = () => {
@@ -15,6 +16,7 @@ const Login = () => {
     const [credentialInfo, setCredentialInfo] = useState<CredentialInfo>({
         email: "",
         password: "",
+        isCheck: false,
     });
     const [errors, setErrors] = useState<{ email?: string; password?: string }>({});
     const [loginError, setLoginError] = useState<string>("");
@@ -23,10 +25,13 @@ const Login = () => {
 
     const handleSignIn = (e: React.FormEvent) => {
         e.preventDefault();
-        const newErrors: { email?: string; password?: string } = {};
-
+        const newErrors: { email?: string; password?: string; } = {};
+        if(!credentialInfo.isCheck){
+            setCredentialInfo({email:"", isCheck:false,password:""});
+            setLoginError("Plaese check the ☑️")
+            return
+        }
         const validationErrors = validateSignIn(credentialInfo.email, credentialInfo.password);
-
         if (Object.keys(validationErrors).length > 0) {
             setErrors(newErrors);
             return;
@@ -37,7 +42,7 @@ const Login = () => {
             setLoginError("");
             navigate("/user")
         } else {
-            setCredentialInfo({ email: "", password: "" });
+            setCredentialInfo({ email: "", password: "", isCheck: false });
             setLoginError("Invalid email or password"); // professional error message
         }
     };
@@ -46,15 +51,15 @@ const Login = () => {
         console.log("Redirect to Sign Up page");
     };
     return (
-        <div className="w-full h-screen flex justify-center items-center bg-gradient-to-r from-blue-50 to-violet-400/20 p-5">
+        <div className="w-full h-screen flex justify-center items-center bg-gradient-to-r from-blue-50 to-violet-400/10 p-5">
             <div className="w-full max-w-md p-8 rounded-2xl bg-white shadow-lg">
                 {/* Heading */}
                 <div className="flex flex-col justify-center items-center">
                     <BiSolidBank size={45} className="text-blue-500" />
-                    <h2 className="text-2xl font-bold text-blue-500 text-center mb-2">Clinic System</h2>
-                    <h2 className="text-mdfont-semibold text-gray-800 text-center mb-2">Sign In</h2>
+                    <h2 className="text-2xl md:text-3xl font-bold text-blue-500 text-center mb-2">Clinic System</h2>
+                    <h2 className="text-sm  text-gray-800 text-center mb-2">sign in to continue</h2>
                 </div>
-                
+
 
                 {/* Form */}
                 <form onSubmit={handleSignIn} autoComplete="off" className="space-y-4">
@@ -63,10 +68,10 @@ const Login = () => {
                         label="Email"
                         type="email"
                         value={credentialInfo.email}
-                        placeholder="you@example.com"
-                        onChange={(e) => { setCredentialInfo({ ...credentialInfo, email: e.target.value }), setLoginError("")}}
+                        placeholder="Username or email"
+                        onChange={(e) => { setCredentialInfo({ ...credentialInfo, email: e.target.value }), setLoginError("") }}
                         required
-                        icon={<FiMail />} // 👈 email icon
+                        icon={<FaUser />} // 👈 email icon
                         error={errors.email} // 👈🏻 error message Email
                     />
 
@@ -77,21 +82,33 @@ const Login = () => {
                         value={credentialInfo.password}
                         placeholder="********"
                         autoComplete="new-password"
-                        onChange={(e) =>{setCredentialInfo({ ...credentialInfo, password: e.target.value }), setLoginError("")}}
+                        onChange={(e) => { setCredentialInfo({ ...credentialInfo, password: e.target.value }), setLoginError("") }}
                         required
-                        icon={<FiLock />} // 👈 lock icon
+                        icon={<FaLock />} // 👈 lock icon
                         error={errors.password} // 👈🏻 error message Password
                     />
 
                     {/* Forgot password */}
                     <div className="flex justify-between">
                         <div className="flex gap-1">
-                            <input type="checkbox" id="remember" className="accent-blue-500" />
-                            <label htmlFor="remember">Remember Me</label>
+                            <input
+                                type="checkbox"
+                                id="remember"
+                                className="accent-blue-500"
+                                checked={credentialInfo.isCheck}
+                                onChange={(e) => {
+                                    setCredentialInfo({
+                                        ...credentialInfo,
+                                        isCheck: e.target.checked,
+                                    });
+                                    setLoginError("");
+                                }}
+                            />
+                            <label htmlFor="remember" className="ml-2">Remember Me</label>
                         </div>
                         <button
                             type="button"
-                            className="text-sm cursor-pointer text-blue-600 hover:underline active:scale-x-95"
+                            className="text-sm  cursor-pointer text-blue-600 hover:underline active:scale-x-95"
                         >
                             Forgot Password?
                         </button>
@@ -111,17 +128,15 @@ const Login = () => {
                         Sign In
                     </button>
                     <p className="text-gray-500 text-center text-sm mb-6">
-                    New here?{" "}
-                    <span
-                        className="text-blue-600 cursor-pointer font-medium active:scale-x-95 hover:underline"
-                        onClick={handleSignUp}
-                    >
-                        Sign Up
-                    </span>
-                </p>
+                        New here?{" "}
+                        <span
+                            className="text-blue-600 cursor-pointer font-medium active:scale-x-95 hover:underline"
+                            onClick={handleSignUp}
+                        >
+                            Sign Up
+                        </span>
+                    </p>
                 </form>
-
-                
             </div>
         </div>
     )
