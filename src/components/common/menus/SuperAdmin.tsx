a8fd4140cf36169867bc25c324385b3c1516fd7b
durgaprasadnxl <durgaprasad@nxltechnologies.com>
2025-09-26 12:29:15 +0530

    to add super admin menu

--- src/components/common/menus/SuperAdmin.tsx
+++ src/components/common/menus/SuperAdmin.tsx
@@ -1,15 +1,28 @@
+import { ChevronDown, LayoutDashboard, Users } from "lucide-react";
+import { useState } from "react";
 
-
-const SuperAdmin:React.FC = () => {
+const SuperAdmin: React.FC = () => {
+  const [dropOpen, setDropOpen] = useState(false);
   return (
-    <div>
-      <h2>welcome</h2>
-      <h2>welcome</h2>
-      <h2>welcome</h2>
-      <h2>welcome</h2>
-      <h2>welcome</h2>
-    </div>
-  )
-}
+    <div className="w-full p-1 overflow-hidden">
+      <div className="w-full p-2 cursor-pointer text-black/80 flex items-center justify-start">
+        <LayoutDashboard className="h-10" strokeWidth={2} />
+        <p className="text-md font-semibold pl-2">Dashboard</p>
+      </div>
 
-export default SuperAdmin
+      <div onClick={()=>setDropOpen(!dropOpen)} className="w-full px-2 py-3 cursor-pointer bg-blue-600 rounded-lg flex items-center justify-between">
+        <Users className="text-white h-5" strokeWidth={2.5} />
+        <p className="text-white text-wrap text-sm">Patient Management</p>
+        <ChevronDown className={`transition-all duration-500 text-white h-5 ${dropOpen?"rotate-90":"rotate-0"}`} strokeWidth={2.5} />
+      </div>
+      <div className={`transition-all duration-500 ${dropOpen? "max-h-40 opacity-100" : "max-h-0 opacity-0"} rounded-lg  bg-white shadow-[0_0_5px_var(--bkShadow)] flex flex-col justify-center gap-1 py-2 pl-4 overflow-hidden w-full`}>
+        <p>New Patient</p>
+        <p>Follow up</p>
+        <p>Pharmacy List</p>
+        <p>Recent Visits</p>
+      </div>
+    </div>
+  );
+};
+
+export default SuperAdmin;
